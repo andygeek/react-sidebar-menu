2aafecf09308387ad99f9a9533c420bfc7efe74e
AndyGeek <andy.eulogio@gmail.com>
2020-10-13 11:16:48 -0500

    I added sidebar data in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
 
 export const Navbar = () => {
-
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
@@ -20,9 +20,19 @@ export const Navbar = () => {
         <ul className="nav-menu-items">
           <li className="navbar-toggle">
             <Link to="#" className="menu-bars">
-              <AiIcons.AiOutlineClose/>
+              <AiIcons.AiOutlineClose />
             </Link>
           </li>
+          {SidebarData.map((item, index) => {
+            return(
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            )
+          })}
         </ul>
       </nav>
     </>
