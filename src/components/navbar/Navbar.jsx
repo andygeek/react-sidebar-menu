b6505a97ba3502c414c6d60db3cd27fb9efe21d2
AndyGeek <andy.eulogio@gmail.com>
2020-10-13 11:36:06 -0500

    Show sidebar and hide sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
+import "./Navbar.css";
 
 export const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
@@ -24,14 +25,14 @@ export const Navbar = () => {
             </Link>
           </li>
           {SidebarData.map((item, index) => {
-            return(
+            return (
               <li key={index} className={item.cName}>
                 <Link to={item.path}>
                   {item.icon}
                   <span>{item.title}</span>
                 </Link>
               </li>
-            )
+            );
           })}
         </ul>
       </nav>
